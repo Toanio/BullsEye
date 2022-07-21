fd42448f9e92f20fce6c27fcb113d1ff9e6039cc
Toan <chan.misha95@mail.ru>
2022-07-21 09:06:51 +0300

    - удалил за комментированные строки

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -21,15 +21,6 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-//        var difference: Int
-//
-//        if currentValue > targetValue {
-//            difference = currentValue - targetValue
-//        }else if targetValue > currentValue {
-//            difference = targetValue - currentValue
-//        }else {
-//            difference = 0
-//        }
         
         let difference = abs(currentValue - targetValue)
         
