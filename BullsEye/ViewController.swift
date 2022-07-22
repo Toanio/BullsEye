476ca9e4776b9bd63a5f60828e54b609afde776c
Toan <chan.misha95@mail.ru>
2022-07-22 17:01:34 +0300

    - Поменял title в попапы и сделал логику получение доп очков

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -28,16 +28,31 @@ class ViewController: UIViewController {
     @IBAction func showAlert() {
         
         let difference = abs(currentValue - targetValue)
-        let points = 100 - difference
+        var points = 100 - difference
         
         let message = "You scored \(points) points"
         
+        
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "Almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pritty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
         score += points
         
-       
-        
         let alert = UIAlertController(
-            title: "Hello World!",
+            title: title,
             message: message,
             preferredStyle: .alert)
         
