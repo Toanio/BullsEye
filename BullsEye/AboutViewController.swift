f7f741f327b393a31610ce48502275299f9f3523
Toan <chan.misha95@mail.ru>
2022-07-25 19:32:45 +0300

    - Добавил кнопку About

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by c.toan on 25.07.2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
